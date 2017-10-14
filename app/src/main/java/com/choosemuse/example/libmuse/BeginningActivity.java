e2c7d8ded248524af9cc954fa66bb1af6f7eae7b
Siyan Li (Sylvia) <lisiyansylvia@gmail.com>
2017-10-14 19:12:40 -0400

    Get Libs

---
+++ app/src/main/java/com/choosemuse/example/libmuse/BeginningActivity.java
@@ -0,0 +1,13 @@
+package com.choosemuse.example.libmuse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeginningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beginning);
+    }
+}
